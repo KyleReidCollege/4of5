46ed05fbdfa90a9e51d558b5753c85af7a5afde3
KyleReidCollege <71388711+KyleReidCollege@users.noreply.github.com>
2021-11-19 17:42:27 +0000

    Added main view code to Engine.h and added Engine.cpp code

--- 4of5/Engine.cpp
+++ 4of5/Engine.cpp
@@ -2,6 +2,33 @@
 
 void Engine::input()
 {
+	sf::Event event;
+	while (m_Window.pollEvent(event))
+	{
+		if (event.type == sf::Event::KeyPressed)
+		{
+			//Handle the player quitting
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			{
+				m_Window.close();
+			}
+			//Handle the player starting the game
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
+			{
+				m_IsPlaying = true;
+			}
+			//Switch between Thomas and Bob
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+			{
+				m_IsCharacter1Selected = !m_IsCharacter1Selected;
+			}
+			//Switch between full and split-screen
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+			{
+				m_IsSplitScreen = !m_IsSplitScreen;
+			}
+		}
+	}
 }
 
 void Engine::update(float dtAsSeconds)
@@ -14,8 +41,46 @@ void Engine::draw()
 
 Engine::Engine()
 {
+	//Get screen resolution and create an SGML window and view
+	sf::Vector2f resolution;
+	resolution.x = sf::VideoMode::getDesktopMode().width;
+	resolution.y = sf::VideoMode::getDesktopMode().height;
+	m_Window.create(sf::VideoMode(resolution.x, resolution.y), "Thomsas Was Late", sf::Style::Fullscreen);
+	
+	//Initialise the fullscreen view
+	m_MainView.setSize(resolution);
+	m_HUDView.reset(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+	//Initialise the split-screen view
+	m_LeftView.setViewport(sf::FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+	m_RightView.setViewport(sf::FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+	m_BGLeftView.setViewport(sf::FloatRect(0.001f, 0.001f, 0.498f, 0.998f));
+	m_BGRightView.setViewport(sf::FloatRect(0.5f, 0.001f, 0.499f, 0.998f));
+	
+	m_BackgroundTexture = TextureHolder::GetTexture("graphics/background.png");
+
+	//Associate the sprite with the texture
+	m_BackgroundSprite.setTexture(m_BackgroundTexture);
 }
 
 void Engine::run()
 {
+	//Timing
+	sf::Clock clock;
+
+	while (m_Window.isOpen())
+	{
+		sf::Time dt = clock.restart();
+
+		//Update the total game time
+		m_GameTimeTotal += dt;
+
+		//Make a decimal fraction from the delta time
+		float dtAsSeconds = dt.asSeconds();
+
+		//Call each part of the game loop in turn
+		input();
+		update(dtAsSeconds);
+		draw();
+	}
 }
--- 4of5/Engine.h
+++ 4of5/Engine.h
@@ -18,33 +18,52 @@ private:
 
 	sf::RenderWindow m_Window;
 
+#pragma region Views
+	//The main views
+	sf::View m_MainView;
+	sf::View m_LeftView;
+	sf::View m_RightView;
+
+	//Three view for the background and  and one HUD view
 	sf::View m_BGMainView;
 	sf::View m_BGLeftView;
 	sf::View m_BGRightView;
 	sf::View m_HUDView;
+#pragma endregion
 
+	//Background sprite and texture
 	sf::Sprite m_BackgroundSprite;
 	sf::Texture m_BackgroundTexture;
 
 #pragma region Booleans
 	//True Booleans
+	//Is character 1 or 2 the current focus?
 	bool m_IsCharacter1Selected = true;
+	//Is it time for a new/first level?
 	bool m_NewLevelRequired = true;
 
 	//False Booleans
+	//Is the game currently playing?
 	bool m_IsPlaying = false;
+	//Is the game in split screen mode?
 	bool m_IsSplitScreen = false;
 #pragma endregion
 
+	//Time left in current level
 	float m_TimeRemaining = 10;
 	sf::Time m_GameTimeTotal;
 
+#pragma region Private Functions
+	//Private function for internal use only
 	void input();
 	void update(float dtAsSeconds);
 	void draw();
+#pragma endregion
 
 public:
+	//The Engine constructor
 	Engine();
+	//Run will call all the private functions
 	void run();
 };
 
